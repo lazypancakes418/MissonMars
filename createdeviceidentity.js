2e09e1aeee80f3ee98f5d7c4fbfe31807371811a
RJ Mohammad <rjmohammad@outlook.com>
2017-05-02 16:39:23 -0400

    finished device ID

--- createdeviceidentity.js
+++ createdeviceidentity.js
@@ -2,7 +2,7 @@
 
 var iothub = require('azure-iothub');
 
-var connectioString ='{iothub connection string}';
+var connectioString ='HostName=coffeepot-mars.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=tWEcUQ5OgYTCwIw09YrlTu9IXPY3l76BlthLkEGrL8U=';
 var registry = iothub.Registry.fromConnectionString(connectioString);
 
 var device = new iothub.Device(null);
@@ -16,3 +16,15 @@ registry.create(device, (err, deviceInfo, res) => {
     printDeviceInfo(err, deviceInfo, res)
   };
 });
+
+printDeviceInfo = (err, deviceInfo, res) =>{
+   if (deviceInfo) {
+     console.log('Device ID: ' + deviceInfo.deviceId);
+     console.log('Device key: ' + deviceInfo.authentication.symmetricKey.primaryKey);
+   }
+ }
+
+/*
+Device ID: myFirstNodeDevice
+Device key: OAQ/5SRv5KFBLmSNAylq8vDhOsrjbw8t6zECngyqcmM=
+*/
