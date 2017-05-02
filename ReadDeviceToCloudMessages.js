71ce883958a22b090b0306421ac57284ae620b0e
RJ Mohammad <rjmohammad@outlook.com>
2017-05-02 16:46:40 -0400

    readDevice to cloud

--- ReadDeviceToCloudMessages.js
+++ ReadDeviceToCloudMessages.js
@@ -1,3 +1,30 @@
 'use strict';
 
- var EventHubClient = require('azure-event-hubs').Client;
+var EventHubClient = require('azure-event-hubs').Client;
+
+var connectioString = 'HostName=coffeepot-mars.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=tWEcUQ5OgYTCwIw09YrlTu9IXPY3l76BlthLkEGrL8U=';
+
+var printerror = (err) => {
+  consle.log(err.message)
+};
+
+var printMessage = (message) => {
+  console.log('Message Received: ');
+  console.log(JSON.stringify(message.body));
+  console.log('');
+};
+
+var client = EventHubClient.fromConnectionString(connectionString);
+client.open()
+  .then(client.getPartitionIds.bind(client))
+  .then(function (partitionIds) {
+    return partitionIds.map(function (partitionId) {
+      return client.createReceiver('$Default', partitionId, { 'startAfterTime': Date.now() }).then(function (receiver) {
+        console.log('Created partition receiver: ' + partitionId)
+        receiver.on('errorReceived', printError);
+        receiver.on('message', printMessage);
+      });
+    });
+  })
+  .catch(printError);
+  
